d2153910513a5b1660b48812b85a22c5055ffb68
John Goodman <john@poeticsystems.com>
2016-06-18 20:53:45 -0500

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-user = CreateAdminService.new.call
-puts 'CREATED ADMIN USER: ' << user.email
-# Environment variables (ENV['...']) can be set in the file .env file.
+user =  User.create(name: 'John Goodman', email: 'john@poeticsystems.com', password: 'Jgpass538', password_confirmation: 'Jgpass538')
+user.admin!
 
 User.create(name: 'Saul Williams', email: 'saul@example.com', password: 'password', password_confirmation: 'password', start_date: '2015-03-10'.to_date)
 User.create(name: 'Lorde', email: 'lorde@example.com', password: 'password', password_confirmation: 'password', start_date: '2016-05-10'.to_date)
