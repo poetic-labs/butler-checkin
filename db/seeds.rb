d13fa72b4052fac23e5ed445a2c178ff8ce2991a
John Goodman <john@poeticsystems.com>
2016-06-18 20:12:34 -0500

    page header

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,5 +9,5 @@ user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
 # Environment variables (ENV['...']) can be set in the file .env file.
 
-User.create(name: 'Saul Williams', email: 'saul@example.com', password: 'password', password_confirmation: 'password')
-User.create(name: 'Lorde', email: 'lorde@example.com', password: 'password', password_confirmation: 'password')
+User.create(name: 'Saul Williams', email: 'saul@example.com', password: 'password', password_confirmation: 'password', start_date: '2015-03-10'.to_date)
+User.create(name: 'Lorde', email: 'lorde@example.com', password: 'password', password_confirmation: 'password', start_date: '2016-05-10'.to_date)
